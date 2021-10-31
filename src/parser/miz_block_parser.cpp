f1a13eb81671209e1597042b95a82bd3477c7ce8
Kazuhisa Nakasho <k.nakasho@gmail.com>
2021-10-31 20:19:12 +0900

    Add statement patterns

--- src/parser/miz_block_parser.cpp
+++ src/parser/miz_block_parser.cpp
@@ -559,30 +559,124 @@ STATEMENT_TYPE
 MizBlockParser::QueryStatementType(KEYWORD_TYPE keyword_type)
 {
     switch (keyword_type) {
-        case KEYWORD_TYPE::ENVIRON:
-            return STATEMENT_TYPE::ENVIRON;
+        case KEYWORD_TYPE::ANTONYM:
+            return STATEMENT_TYPE::ANTONYM;
+        case KEYWORD_TYPE::ASSOCIATIVITY:
+            return STATEMENT_TYPE::ASSOCIATIVITY;
+        case KEYWORD_TYPE::ASSUME:
+            return STATEMENT_TYPE::ASSUME;
+        case KEYWORD_TYPE::ASYMMETRY:
+            return STATEMENT_TYPE::ASYMMETRY;
+        case KEYWORD_TYPE::ATTR:
+            return STATEMENT_TYPE::ATTR;
         case KEYWORD_TYPE::BEGIN_:
-            return STATEMENT_TYPE::SECTION;
-        case KEYWORD_TYPE::VOCABULARIES:
-            return STATEMENT_TYPE::VOCABULARIES;
-        case KEYWORD_TYPE::NOTATIONS:
-            return STATEMENT_TYPE::NOTATIONS;
+            return STATEMENT_TYPE::BEGIN;
+        case KEYWORD_TYPE::CLUSTER:
+            return STATEMENT_TYPE::CLUSTER;
+        case KEYWORD_TYPE::COHERENCE:
+            return STATEMENT_TYPE::COHERENCE;
+        case KEYWORD_TYPE::COMMUTATIVITY:
+            return STATEMENT_TYPE::COMMUTATIVITY;
+        case KEYWORD_TYPE::CONNECTEDNESS:
+            return STATEMENT_TYPE::CONNECTEDNESS;
+        case KEYWORD_TYPE::CONSIDER:
+            return STATEMENT_TYPE::CONSIDER;
+        case KEYWORD_TYPE::CONSISTENCY:
+            return STATEMENT_TYPE::CONSISTENCY;
         case KEYWORD_TYPE::CONSTRUCTORS:
             return STATEMENT_TYPE::CONSTRUCTORS;
-        case KEYWORD_TYPE::REGISTRATIONS:
-            return STATEMENT_TYPE::REGISTRATIONS;
+        case KEYWORD_TYPE::CORRECTNESS:
+            return STATEMENT_TYPE::CORRECTNESS;
+        case KEYWORD_TYPE::DEFFUNC:
+            return STATEMENT_TYPE::DEFFUNC;
         case KEYWORD_TYPE::DEFINITIONS:
             return STATEMENT_TYPE::DEFINITIONS;
+        case KEYWORD_TYPE::DEFPRED:
+            return STATEMENT_TYPE::DEFPRED;
+        case KEYWORD_TYPE::ENVIRON:
+            return STATEMENT_TYPE::ENVIRON;
+        case KEYWORD_TYPE::EX:
+            return STATEMENT_TYPE::EX;
+        case KEYWORD_TYPE::EXISTENCE:
+            return STATEMENT_TYPE::EXISTENCE;
         case KEYWORD_TYPE::EXPANSIONS:
             return STATEMENT_TYPE::EXPANSIONS;
         case KEYWORD_TYPE::EQUALITIES:
             return STATEMENT_TYPE::EQUALITIES;
-        case KEYWORD_TYPE::THEOREMS:
-            return STATEMENT_TYPE::THEOREMS;
-        case KEYWORD_TYPE::SCHEMES:
-            return STATEMENT_TYPE::SCHEMES;
+        case KEYWORD_TYPE::FOR:
+            return STATEMENT_TYPE::FOR;
+        case KEYWORD_TYPE::FUNC:
+            return STATEMENT_TYPE::FUNC;
+        case KEYWORD_TYPE::GIVEN:
+            return STATEMENT_TYPE::GIVEN;
+        case KEYWORD_TYPE::HENCE:
+            return STATEMENT_TYPE::HENCE;
+        case KEYWORD_TYPE::IDEMPOTENCE:
+            return STATEMENT_TYPE::IDEMPOTENCE;
+        case KEYWORD_TYPE::IDENTIFY:
+            return STATEMENT_TYPE::IDENTIFY;
+        case KEYWORD_TYPE::INVOLUTIVENESS:
+            return STATEMENT_TYPE::INVOLUTIVENESS;
+        case KEYWORD_TYPE::IRREFLEXIVITY:
+            return STATEMENT_TYPE::IRREFLEXIVITY;
+        case KEYWORD_TYPE::LET:
+            return STATEMENT_TYPE::LET;
+        case KEYWORD_TYPE::MODE:
+            return STATEMENT_TYPE::MODE;
+        case KEYWORD_TYPE::NOTATIONS:
+            return STATEMENT_TYPE::NOTATIONS;
+        case KEYWORD_TYPE::PER:
+            return STATEMENT_TYPE::PER_CASES;
+        case KEYWORD_TYPE::PRED:
+            return STATEMENT_TYPE::PRED;
+        case KEYWORD_TYPE::PROJECTIVITY:
+            return STATEMENT_TYPE::PROJECTIVITY;
+        case KEYWORD_TYPE::RECONSIDER:
+            return STATEMENT_TYPE::RECONSIDER;
+        case KEYWORD_TYPE::REDUCE:
+            return STATEMENT_TYPE::REDUCE;
+        case KEYWORD_TYPE::REDUCIBILITY:
+            return STATEMENT_TYPE::REDUCIBILITY;
+        case KEYWORD_TYPE::REDEFINE:
+            return STATEMENT_TYPE::REDEFINE;
+        case KEYWORD_TYPE::REFLEXIVITY:
+            return STATEMENT_TYPE::REFLEXIVITY;
+        case KEYWORD_TYPE::REGISTRATIONS:
+            return STATEMENT_TYPE::REGISTRATIONS;
         case KEYWORD_TYPE::REQUIREMENTS:
             return STATEMENT_TYPE::REQUIREMENTS;
+        case KEYWORD_TYPE::RESERVE:
+            return STATEMENT_TYPE::RESERVE;
+        case KEYWORD_TYPE::SCHEME:
+            return STATEMENT_TYPE::SCHEME;
+        case KEYWORD_TYPE::SCHEMES:
+            return STATEMENT_TYPE::SCHEMES;
+        case KEYWORD_TYPE::SECTION:
+            return STATEMENT_TYPE::SECTION;
+        case KEYWORD_TYPE::SET:
+            return STATEMENT_TYPE::SET;
+        case KEYWORD_TYPE::SETHOOD:
+            return STATEMENT_TYPE::SETHOOD;
+        case KEYWORD_TYPE::STRUCT:
+            return STATEMENT_TYPE::STRUCT;
+        case KEYWORD_TYPE::SYMMETRY:
+            return STATEMENT_TYPE::SYMMETRY;
+        case KEYWORD_TYPE::SYNONYM:
+            return STATEMENT_TYPE::SYNONYM;
+        case KEYWORD_TYPE::TAKE:
+            return STATEMENT_TYPE::TAKE;
+        case KEYWORD_TYPE::THEOREM:
+            return STATEMENT_TYPE::THEOREM;
+        case KEYWORD_TYPE::THEOREMS:
+            return STATEMENT_TYPE::THEOREMS;
+        case KEYWORD_TYPE::THUS:
+            return STATEMENT_TYPE::THUS;
+        case KEYWORD_TYPE::TRANSITIVITY:
+            return STATEMENT_TYPE::TRANSITIVITY;
+        case KEYWORD_TYPE::UNIQUENESS:
+            return STATEMENT_TYPE::UNIQUENESS;
+        case KEYWORD_TYPE::VOCABULARIES:
+            return STATEMENT_TYPE::VOCABULARIES;
         default:
             return STATEMENT_TYPE::UNKNOWN;
     }
